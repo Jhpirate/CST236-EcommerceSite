b53b41be1a1a76201694a1ae53eb720ae56f1d5c
Jared Heeringa <jaredheeringa@icloud.com>
2021-02-03 21:42:48 -0700

    (Temporary) update home page with links for quick access to modules

--- index.php
+++ index.php
@@ -1,19 +1,31 @@
-<?php
-
-echo "<h1>" . "Welcome to my CST236 ECommerce Application" . "</h1>";
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Home</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
 </head>
 <body>
 
-<a href="Presentation/Views/Login/login.html">Login.html</a>
-<a href="Presentation/Views/Register/register.html">Register.html</a>
+<div class="container is-light">
+    <section class="section">
+        <div class="title is-1">
+            <h1><strong>CST236</strong> Ecommerce Site</h1>
+        </div>
+    </section>
+</div>
+<hr>
+<section class="section">
+    <div class="container">
+        <ul class="menu-list">
+            <li><a href="Presentation/Views/Login/login.html">Login.html</a></li>
+            <li><a href="Presentation/Views/Register/register.html">Register.html</a></li>
+            <li><a href="Presentation/Views/Search/search.html">Search.html</a></li>
+        </ul>
+    </div>
+</section>
+<!--<a href="Presentation/Views/Login/login.html">Login.html</a>-->
+<!--<a href="Presentation/Views/Register/register.html">Register.html</a>-->
 
 </body>
 </html>
