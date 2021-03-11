9d9454e4e5212f30ac685cca99b2cd837a6ec76c
Jared Heeringa <jaredheeringa@icloud.com>
2021-03-10 20:55:51 -0700

    directory test for production.

---
+++ Presentation/Views/TestPages/phpDirectoryTest.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * Copyright (c) 2021. Jared Heeringa - GCU CST236 Ecommerce Project
+ */
+
+echo "Document Root is: " . $_SERVER["DOCUMENT_ROOT"];
+echo "<br>";
+echo "ScriptFIleName: " . $_SERVER["SCRIPT_FILENAME"];
