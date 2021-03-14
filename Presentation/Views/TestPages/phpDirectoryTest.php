07f66da593ae5b68eac70405538713c00ef43e72
Jared Heeringa <jaredheeringa@icloud.com>
2021-03-14 16:24:23 -0700

    test file

--- Presentation/Views/TestPages/phpDirectoryTest.php
+++ Presentation/Views/TestPages/phpDirectoryTest.php
@@ -6,3 +6,20 @@
 echo "Document Root is: " . $_SERVER["DOCUMENT_ROOT"];
 echo "<br>";
 echo "ScriptFIleName: " . $_SERVER["SCRIPT_FILENAME"];
+echo "<br>";
+echo "PATH INFO: " . $_SERVER["PATH_INFO"];
+echo "<br>";
+echo "REQUEST URI: " . $_SERVER["REQUEST_URI"];
+echo "<br>";
+echo "PHP_SELF: " . $_SERVER["PHP_SELF"];
+
+echo "<br>";
+echo "<br>";
+
+$path = dirname(__FILE__) . DIRECTORY_SEPARATOR;
+echo "Path: " . $path;
+?>
+
+
+
+<a href="/">whereugo</a>
