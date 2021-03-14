d537d98ebca1c84ef902d4fe83fb16a5705ea1df
Jared Heeringa <jaredheeringa@icloud.com>
2021-03-14 16:20:04 -0700

    only show active, non-deleted products

--- Presentation/Handlers/_displayProductResults.php
+++ Presentation/Handlers/_displayProductResults.php
@@ -27,21 +27,24 @@
     for ($i = 0; $i < count($productList); $i++) {
         $currentProductID = $productList[$i]["ID"];
 
-        echo "<tr>";
+        // Only output active products
+        if($productList[$i]["deleted"] != 1) {
+            echo "<tr>";
 
-        echo "<td>" . $currentProductID . "</td>";
-        echo "<td>" . $productList[$i]["product_name"] . "</td>";
-        echo "<td>" . $productList[$i]["product_description"] . "</td>";
-        echo "<td>" . $productList[$i]["size"] . "</td>";
-        echo "<td>" . $productList[$i]["color"] . "</td>";
-        echo "<td>" . $productList[$i]["price"] . "</td>";
+            echo "<td>" . $currentProductID . "</td>";
+            echo "<td>" . $productList[$i]["product_name"] . "</td>";
+            echo "<td>" . $productList[$i]["product_description"] . "</td>";
+            echo "<td>" . $productList[$i]["size"] . "</td>";
+            echo "<td>" . $productList[$i]["color"] . "</td>";
+            echo "<td>" . $productList[$i]["price"] . "</td>";
 
-        // Edit button
-        echo "<td>";
-        echo "<form action='../Views/ProductPage/viewProduct.php' method='get'><input type='hidden' name='productID' value='$currentProductID'><input class='button is-primary' type='submit' value='View More...'></form>";
-        echo "</td>";
+            // Edit button
+            echo "<td>";
+            echo "<form action='../Views/ProductPage/viewProduct.php' method='get'><input type='hidden' name='productID' value='$currentProductID'><input class='button is-primary' type='submit' value='View More...'></form>";
+            echo "</td>";
 
-        echo "</tr>" . PHP_EOL;
+            echo "</tr>" . PHP_EOL;
+        }
     }
 
     ?>
