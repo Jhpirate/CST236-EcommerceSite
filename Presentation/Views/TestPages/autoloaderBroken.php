a8e467674288f1a7effed4c7586e00bae261d4db
Jared Heeringa <jaredheeringa@icloud.com>
2021-03-24 16:41:28 -0700

    testing autoloader functionality (session corrupted causing it to no longer work)

---
+++ Presentation/Views/TestPages/autoloaderBroken.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Copyright (c) 2021. Jared Heeringa - GCU CST236 Ecommerce Project
+ */
+
+include_once "../../../Utility/autoloader.php";
+
+echo $_SERVER["DOCUMENT_ROOT"];
+
+$file = $_SERVER["DOCUMENT_ROOT"] . "/BusinessServices/ShoppingCart.php";
+
+echo "<br>FilePath: " . $file;
+
+if(file_exists($file)) {
+    echo "<br>Valid";
+} else {
+    echo "<br>Not Valid";
+}
+
+phpinfo();
