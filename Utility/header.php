42c1e4be5ab0e043ad52ce85d7085bba5a7a84cf
Jared Heeringa <jaredheeringa@icloud.com>
2021-03-29 16:10:34 -0700

    set session storage location. Normal directory became read only? IDK why and it resets even after taking ownership.

--- Utility/header.php
+++ Utility/header.php
@@ -2,5 +2,6 @@
 /*
  * Copyright (c) 2021. Jared Heeringa - GCU Project
  */
-
+$sessionPath = $_SERVER["DOCUMENT_ROOT"] . "/Utility/phpSessionTemp";
+session_save_path($sessionPath);
 session_start();
