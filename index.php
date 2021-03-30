fee3590915fdac7838324f635373192e84de332f
Jared Heeringa <jaredheeringa@icloud.com>
2021-03-29 20:49:08 -0700

    Added more stuff to home page to make it look better and more full :)

--- index.php
+++ index.php
@@ -23,12 +23,26 @@
     <div class="container">
         <div class="columns">
             <div class="column">
-                <h1 class="title">Test Col 1</h1>
-                <h1 class="subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua. Malesuada pellentesque elit eget gravida cum sociis natoque penatibus.</h1>
+                <h1 class="title">
+                    Test Col 1
+                </h1>
+                <h1 class="subtitle">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Malesuada pellentesque elit eget gravida cum sociis natoque penatibus.
+                </h1>
+
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-3by2">
+                            <img src="https://picsum.photos/600/600" alt="cannot connect to picsum.photos to get a random picture :(">
+                        </figure>
+                    </div>
+                </div>
+
             </div>
             <div class="column">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
                     et dolore magna aliqua. Malesuada pellentesque elit eget gravida cum sociis natoque penatibus.
                     Mollis aliquam ut porttitor leo a. Nibh praesent tristique magna sit amet purus gravida quis
                     blandit. Tristique senectus et netus et. Sed enim ut sem viverra aliquet. Aliquet risus feugiat in
@@ -38,13 +52,111 @@
                     diam maecenas sed enim. Lacus luctus accumsan tortor posuere ac ut consequat semper viverra.
                     Placerat vestibulum lectus mauris ultrices eros. Aliquet risus feugiat in ante metus dictum at
                     tempor. Penatibus et magnis dis parturient montes nascetur ridiculus mus mauris. Id neque aliquam
-                    vestibulum morbi blandit cursus risus at.</p>
+                    vestibulum morbi blandit cursus risus at.
+                </p>
             </div>
         </div>
     </div>
 </section>
-<!--<a href="Presentation/Views/Login/login.html">Login.html</a>-->
-<!--<a href="Presentation/Views/Register/register.html">Register.html</a>-->
+
+<section class="section">
+    <div class="container">
+        <div class="columns">
+            <div class="column">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Malesuada pellentesque elit eget gravida cum sociis natoque penatibus.
+                    Mollis aliquam ut porttitor leo a. Nibh praesent tristique magna sit amet purus gravida quis
+                    blandit. Tristique senectus et netus et. Sed enim ut sem viverra aliquet. Aliquet risus feugiat in
+                    ante metus. Augue neque gravida in fermentum et sollicitudin ac. Viverra orci sagittis eu volutpat
+                    odio facilisis. At in tellus integer feugiat scelerisque varius. Faucibus vitae aliquet nec
+                    ullamcorper sit. Odio aenean sed adipiscing diam donec adipiscing. Vitae suscipit tellus mauris a
+                    diam maecenas sed enim. Lacus luctus accumsan tortor posuere ac ut consequat semper viverra.
+                    Placerat vestibulum lectus mauris ultrices eros. Aliquet risus feugiat in ante metus dictum at
+                    tempor. Penatibus et magnis dis parturient montes nascetur ridiculus mus mauris. Id neque aliquam
+                    vestibulum morbi blandit cursus risus at.
+                </p>
+            </div>
+            <div class="column">
+                <h1 class="title">
+                    Test Content #2
+                </h1>
+                <h1 class="subtitle">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Malesuada pellentesque elit eget gravida cum sociis natoque penatibus.
+                </h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section">
+    <div class="container">
+
+        <div class="heading">
+            <h1 class="title">Featured Images</h1>
+            <h5 class="subtitle">See some our best items below</h5>
+        </div>
+
+
+        <div class="columns">
+            <!-- Image 1 -->
+            <div class="column">
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img src="https://picsum.photos/501/501" alt="cannot connect to picsum.photos to get a random picture :(">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="content">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                                et dolore magna aliqua. Malesuada pellentesque elit eget gravida cum sociis natoque penatibus.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Image 2 -->
+            <div class="column">
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img src="https://picsum.photos/500/500" alt="cannot connect to picsum.photos to get a random picture :(">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="content">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                                et dolore magna aliqua. Malesuada pellentesque elit eget gravida cum sociis natoque penatibus.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Image 3 -->
+            <div class="column">
+                <div class="card">
+                    <div class="card-image">
+                        <figure class="image is-4by3">
+                            <img src="https://picsum.photos/502/502" alt="cannot connect to picsum.photos to get a random picture :(">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="content">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                                et dolore magna aliqua. Malesuada pellentesque elit eget gravida cum sociis natoque penatibus.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 </body>
 
